33f96731bfcd564df404270446b19d8c0203894f
Jason Ward <jason.louard.ward@gmail.com>
2022-12-29 16:39:32 -0500

    Added player onto screen.

--- main.py
+++ main.py
@@ -10,12 +10,25 @@ pygame.display.set_caption('Noob Game')
 icon = pygame.image.load('spaceship.png')
 pygame.display.set_icon(icon)
 
+# Player
+player_image = pygame.image.load('player.png')
+player_x = 370
+player_y = 480
+
+
+def player():
+    screen.blit(player_image, (player_x, player_y))
+
+
 # Game Loop
 running = True
 while running:
+    # RGB - (Red, Green, Blue)
+    screen.fill((255, 255, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((255, 255, 0))
+    player()
     pygame.display.update()
